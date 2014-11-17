7b71662ad924eb4344b110bc5811b204975def6d
ILYA Khlopotov <ilya.khlopotov@gmail.com>
2014-11-17 13:16:01 -0800

    Update emacs to version 24.3.x

--- emacs/emacs/etc/java.el
+++
@@ -1,8 +0,0 @@
-(defun init-java ()
-  (message "Initializing java")
-  (setq-default tab-width 4
-		;; Use spaces instead of tabs
-		indent-tabs-mode nil) 
-  (add-hook 'java-mode 'delete-trailing-whitespace-hook))
-
-(add-hook 'java-mode-hook 'init-java)
